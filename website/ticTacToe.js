d284d3dde75d486fff0925a61f9d9c1d1d4fee16
Donovan Brocious <110584158+donovan-creator@users.noreply.github.com>
2023-10-03 10:23:35 -0400

    Add files via upload

---
+++ website/ticTacToe.js
@@ -0,0 +1,10 @@
+let boardArray1 = [0,0,0,];
+let boardArray2 = [0,0,0,];
+let boardArray3 = [0,0,0,];
+
+let player1 = prompt("Player 1 enter your name");
+let player2 = prompt("Player 2 enter your name");
+
+document.getElementById("board1").innerHTML=boardArray1
+
+console.log(boardArray1,'\n',boardArray2,'\n',boardArray3);
